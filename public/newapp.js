dde27fb4bca192d41f92172d3061c4732f2dc414
Colton Savage <csavage1994@gmail.com>
2016-02-15 14:55:26 -0600

    implemented jquery and entity constructor

--- public/newapp.js
+++ public/newapp.js
@@ -1,4 +1,14 @@
-document.querySelector('#cube').addEventListener('click', function () {
+/*document.querySelector('#cube1').addEventListener('click', function () {
   this.setAttribute('material', 'color', 'red');
   console.log('I was clicked!');
+});*/
+var createEntity = function(pos, id, geometry, material){
+  return '<a-entity position="' + pos[0] + ' ' + pos[1] + ' ' + pos[2] + '"id="' + id + '" geometry ="primitive: ' + geometry + '" material="color: ' + ' ' + material +'"></a-entity>';
+};
+
+$('#cube1').on('click', function(){
+  this.setAttribute('material', 'color', 'red');
 });
+var cubeTest = createEntity([3,3,3], 'cube4', 'box', 'green');
+console.log(cubeTest);
+$('#scene').append(cubeTest);
