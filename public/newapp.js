656654953b693d70a98ba44f6f0cf49197677695
Colton Savage <csavage1994@gmail.com>
2016-02-15 15:15:07 -0600

    finished first puzzle and added score tracking

--- public/newapp.js
+++ public/newapp.js
@@ -2,13 +2,47 @@
   this.setAttribute('material', 'color', 'red');
   console.log('I was clicked!');
 });*/
-var createEntity = function(pos, id, geometry, material){
-  return '<a-entity position="' + pos[0] + ' ' + pos[1] + ' ' + pos[2] + '"id="' + id + '" geometry ="primitive: ' + geometry + '" material="color: ' + ' ' + material +'"></a-entity>';
-};
+$(document).ready(function(){
+  testOne = [0, 0, 0];
 
-$('#cube1').on('click', function(){
-  this.setAttribute('material', 'color', 'red');
+  var createEntity = function(pos, id, geometry, material){
+    return '<a-entity position="' + pos[0] + ' ' + pos[1] + ' ' + pos[2] + '"id="' + id + '" geometry ="primitive: ' + geometry + '" material="color: ' + ' ' + material +'"></a-entity>';
+  };
+
+  var increaseScore = function(){
+    var score = parseInt($('#int').text());
+    score += 1000;
+    $('#int').text(score);
+  }
+  var puz1cube1 = createEntity([3,0,0], 'puz1Cube1', 'box', 'blue');
+  var puz1cube2 = createEntity([0,0,0], 'puz1Cube2', 'box', 'green');
+  var puz1cube3 = createEntity([-3,0,0], 'puz1Cube3', 'box', 'teal');
+  $('#scene').append(puz1cube1, puz1cube2, puz1cube3);
+
+  $('#puz1Cube1').on('click', function(){
+    this.setAttribute('material', 'color', '#00ff00');
+    testOne[0] = 1;
+    if(testOne.indexOf(0) === -1){
+      console.log('complete');
+      increaseScore();
+    }
+  });
+
+  $('#puz1Cube2').on('click', function(){
+    this.setAttribute('material', 'color', '#00ff00');
+    testOne[1] = 1;
+    if(testOne.indexOf(0) === -1){
+      console.log('complete');
+      increaseScore();
+    }
+  });
+
+  $('#puz1Cube3').on('click', function(){
+    this.setAttribute('material', 'color', '#00ff00');
+    testOne[2] = 1;
+    if(testOne.indexOf(0) === -1){
+      console.log('complete');
+      increaseScore();
+    }
+  });
 });
-var cubeTest = createEntity([3,3,3], 'cube4', 'box', 'green');
-console.log(cubeTest);
-$('#scene').append(cubeTest);
